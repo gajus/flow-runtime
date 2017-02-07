ad619b3e633599ee699b8ef5092a40f74fe43df9
Charles Pick <charles@codemix.com>
2017-02-07 21:51:23 +0000

    don't minify browser build

--- packages/flow-runtime/config/rollup.config.browser.js
+++ packages/flow-runtime/config/rollup.config.browser.js
@@ -2,8 +2,6 @@
 
 import babel from 'rollup-plugin-babel';
 import nodeResolve from 'rollup-plugin-node-resolve';
-import uglify from 'rollup-plugin-uglify';
-import {minify} from 'uglify-js';
 
 export default {
   entry: 'src/index.cjs.js',
@@ -30,10 +28,9 @@ export default {
     nodeResolve({
       // not all files you want to resolve are .js files
       extensions: [ '.js', '.json' ]
-    }),
-    uglify({}, minify)
+    })
   ],
   targets: [
-    { dest: 'dist/flow-runtime.min.js', format: 'umd' },
+    { dest: 'dist/flow-runtime.umd.js', format: 'umd' },
   ]
 };
