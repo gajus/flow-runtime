0548a6f147d8993b76c9250fd38f0ce7446c7f77
Charles Pick <charles@codemix.com>
2017-03-16 22:43:31 +0000

    fix broken test

--- packages/flow-runtime/src/__tests__/__fixtures__/bugs/73-class-t.js
+++ packages/flow-runtime/src/__tests__/__fixtures__/bugs/73-class-t.js
@@ -27,13 +27,26 @@ export function pass (t: TypeContext) {
 }
 
 
-export function failz (t: TypeContext) {
-  const ErrorType = t.object(
-    t.staticCallProperty(t.function(t.param('message', t.string(), true))),
-    t.property('message', t.string()),
-  );
-  const err = Object.create(null);
-  // @flowIgnore
-  err.message = 'nope';
-  return ErrorType.assert(err);
+export function fail (t: TypeContext) {
+  const HasKey = t.type('HasKey', t.object(t.property('getKey', t.function(t.return(t.string())))));
+
+  @t.annotate(t.class('Klazz', t.method('getKey', t.return(t.number()))))
+  class Klazz {
+    getKey() {
+      const _returnType = t.return(t.string());
+
+      return _returnType.assert('key');
+    }
+  }
+
+  const logKey = t.annotate(function logKey(C) {
+    let _CType = t.Class(HasKey);
+
+    t.param('C', _CType).assert(C);
+
+    const instance = new C();
+    return instance.getKey();
+  }, t.function(t.param('C', t.Class(HasKey))));
+
+  return logKey(Klazz);
 }
