d1a2515756c15268bc4097450516e07e81343b5b
Andy Edwards <jedwards@fastmail.com>
2018-01-22 09:40:21 -0600

    include babel-preset-env in getting started docs
    fix #183

--- packages/flow-runtime-docs/src/components/docs/GettingStartedPage.js
+++ packages/flow-runtime-docs/src/components/docs/GettingStartedPage.js
@@ -7,7 +7,7 @@ import InstallInstruction from '../InstallInstruction';
 
 const babelConfig = `
 {
-  "presets": ["stage-2", "react"],
+  "presets": ["env", "stage-2", "react"],
   "plugins": [["flow-runtime"], "transform-decorators-legacy"]
 }
 `.trim();
@@ -27,6 +27,7 @@ export default class GettingStartedPage extends Component {
           <InstallInstruction packageNames={['flow-runtime']}
                               devPackageNames={[
                                 'babel-plugin-flow-runtime',
+                                'babel-preset-env',
                                 'babel-preset-stage-2',
                                 'babel-preset-react',
                                 'babel-plugin-transform-decorators-legacy'
@@ -38,6 +39,7 @@ export default class GettingStartedPage extends Component {
           <p>Add the following to your babel configuration or <code>.babelrc</code> file:</p>
           <pre>{babelConfig}</pre>
           <p>This will include the plugin with the default configuration, see the <Link to="/babel-plugin-flow-runtime">babel-plugin-flow-runtime</Link> page for more information about the plugin options.</p>
+          <p className="text-muted">You may use old babel presets like <code>babel-preset-es2015</code> in place of <code>babel-preset-env</code>, as long as they are configured to transpile <code>import</code> statements.</p>
         </div>
       </div>
     );
