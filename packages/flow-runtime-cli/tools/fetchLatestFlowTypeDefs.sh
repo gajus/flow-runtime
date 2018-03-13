b2764ebf681e276ec519751b3b185eecacab36ae
Christophe Hurpeau <christophe.hurpeau+github@gmail.com>
2018-03-13 12:22:39 +0100

    flow-runtime-cli: fetchLatestFlowTypeDefs fix move flow-builtins path

--- packages/flow-runtime-cli/tools/fetchLatestFlowTypeDefs.sh
+++ packages/flow-runtime-cli/tools/fetchLatestFlowTypeDefs.sh
@@ -6,6 +6,6 @@ git remote add origin https://github.com/facebook/flow.git
 git config core.sparsecheckout true
 echo "lib/*" >> .git/info/sparse-checkout
 git pull --depth=1 origin master
-mv ./lib/* ../flow-builtins
+mv ./lib/* ../../flow-builtins
 cd ..
 rm -rf ./flow-builtins-tmp
