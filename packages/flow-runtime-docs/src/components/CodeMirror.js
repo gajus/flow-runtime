de81556e208124238dfc43a30ed4b6413b04bada
mhatvan <markus_hatvan@aon.at>
2020-07-09 23:37:20 +0200

    refactor: migrate to react-codemirror2 (#143)
    - remove react-codemirror in favor of react-codemirror2
    - switch to react-codemirror2 in CodeMirror.js and CodeOutput.js
    
    Signed-off-by: mhatvan <markus_hatvan@aon.at>

--- packages/flow-runtime-docs/src/components/CodeMirror.js
+++ packages/flow-runtime-docs/src/components/CodeMirror.js
@@ -3,20 +3,19 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 
-import ReactCodeMirror from 'react-codemirror';
+import {UnControlled as ReactCodeMirror} from 'react-codemirror2';
 import 'codemirror/mode/javascript/javascript';
 import 'codemirror/lib/codemirror.css';
 
 type Props = {
-  onChange?: (code: string) => any;
-  readOnly?: boolean;
-  value: string;
-  mode?: string;
+  onChange?: (code: string) => any,
+  readOnly?: boolean,
+  value: string,
+  mode?: string,
 };
 
 @observer
 export default class CodeMirror extends React.Component<Props, void> {
-
   handleChange = (code: string) => {
     const {onChange} = this.props;
     if (onChange) {
@@ -24,17 +23,23 @@ export default class CodeMirror extends React.Component<Props, void> {
     }
   };
 
-  render () {
+  render() {
     const {mode, value, readOnly} = this.props;
     const options = {
       lineNumbers: true,
       mode: mode || 'javascript',
       tabSize: 2,
       readOnly: readOnly ? true : false,
-      viewportMargin: Infinity
+      viewportMargin: Infinity,
     };
     return (
-        <ReactCodeMirror value={value} onChange={this.handleChange} options={options} />
+      <ReactCodeMirror
+        value={value}
+        onChange={(editor, data, value) => this.handleChange(value)}
+        options={options}
+        autoCursor={false}
+        autoScroll={false}
+      />
     );
   }
 }
